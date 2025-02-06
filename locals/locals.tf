41021adf71c7b7c694d92b45245ab4d5896f2dc7
Aswini306 <ashupuri5@gmail.com>
2025-02-06 11:43:22 +0530

    Update locals.tf

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,6 +1,6 @@
 locals {
-    domain_name  = "daws81s.online"
-    zone_id = "Z09912121MS725XSKH1TG"
+    domain_name  = "aswini.blog"
+    zone_id = "Z0409476WUE29DSSPESQ"
     instance_type = var.environment == "prod" ? "t3.medium" : "t3.micro"
     # count.index will not work in locals
 }
